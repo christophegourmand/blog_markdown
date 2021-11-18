28fb3a2a0422331ab525c8f17687ecc280129136
christophegourmand <christophe.gourmand@gmail.com>
2021-11-18 12:49:55 +0100

    add router for articles
    new file:   routes/articles.js
    - configure a router for uri `/articles`
    (every route configured in this file will be
    accessible at the end of uri `/articles` + `/....`)
    - send a response message at uri `/articles` +`/`
    - export articlesRouter
    
    modified:   server.js
    - import articleRouter
    - told `app` to use `articleRouter` at uri `/articles`

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 // CONFIGURE Express.js
 const express = require('express');
+const articleRouter = require('./routes/articles');
 const app = express();
 
 // configure view engine so EJS compile __.ejs into HTML files
 app.set('view engine', 'ejs');
+
+// every route in our articleRouter will be created at the end of uri '/articles/'
+app.use('/articles', articleRouter)
 
 // ROUTES
 app.get(
