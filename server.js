6649d1553e73f62334515d45e01749fe8e107ca2
christophegourmand <christophe.gourmand@gmail.com>
2021-11-18 12:40:32 +0100

    add index page on route '/'
    modified:   server.js
    - change message "hello world" from route '/' to
    route '/hello'
    - configure route '/' to render `views/index.ejs`
    
    new file:   views/index.ejs
    - created a HTML basic structure

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
+// CONFIGURE Express.js
 const express = require('express');
 const app = express();
 
+// configure view engine so EJS compile __.ejs into HTML files
+app.set('view engine', 'ejs');
+
+// ROUTES
 app.get(
-    '/',
+    '/hello',
     (req, res)=> {
         res.send('Hello World');
     }
 );
+
+app.get(
+    '/',
+    (req, res)=> {
+        res.render('index');
+    }
+);
+
 
 app.listen(5000);
